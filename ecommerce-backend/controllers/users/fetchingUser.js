191825efb399689d04f003e4a316f52d5e2477f9
Gowtham Raj <gmanda@gitam.in>
2024-07-25 01:04:36 +0530

    fetchUser

--- ecommerce-backend/controllers/users/fetchingUser.js
+++ ecommerce-backend/controllers/users/fetchingUser.js
@@ -2,9 +2,9 @@ const usersSchema = require('../../models/users/usersSchema');
 
 const fetchingUser = async (req, res) => {
     try{
-        const token = window.localStorage.getItem("token");
-        const user = await usersSchema.findOne({token : token});
-        res.status(200).json({message : "User fetched successfully" , user : user});
+        const user = await usersSchema.findOne({userId : req.body.userID});
+        console.log(user);
+        res.status(200).json({user});
     }
     catch(err){
         res.status(500).json({message : "Error Fetching User"});
--- ecommerce-backend/routers/user.js
+++ ecommerce-backend/routers/user.js
@@ -7,6 +7,7 @@ const sendOtp = require('../controllers/users/sendOtp');
 const verifyOtp = require('../controllers/users/verifyOtp');
 const addAddress = require('../controllers/users/addAddress');
 const fetchUserByToken = require('../controllers/users/fetchUserByToken');
+const  fetchingUser = require('../controllers/users/fetchingUser');
 
 routes
     .post('/addUser', addUser.addUser)
@@ -14,6 +15,7 @@ routes
     .post('/loginUser', loginUser.loginUser)
     .post('/sendOtp', sendOtp.sendOtp)
     .post('/addAddress', addAddress.addAddress)
+    .post('/fetchingUser' , fetchingUser.fetchingUser)
     .post('/fetchUserByToken', fetchUserByToken.fetchUserByToken)
     .post('/verifyOtp', verifyOtp.verifyOtp);
 
